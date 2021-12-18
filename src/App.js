768f9c572ea503c71b334f9cd2c263bed033ecf4
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2021-12-18 18:40:33 -0300

    Add base code for the app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [newName, setNewName] = useState('');
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+    </div>
+  );
+}
+
+export default App;
