949dcd69cf7a9ac269f410b527f302388800649b
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-01-25 12:47:55 -0300

    Add comment

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,21 +1,23 @@
 import axios from 'axios'
+
+/* Proviene del deploy de mi API */
 const baseURL = 'https://protected-tor-63201.herokuapp.com/api/persons'
 
-const getAll = () => axios.get(baseURL)
+export const getAll = () => axios.get(baseURL)
 
-const create = newContact => {
+export const create = newContact => {
   const request = axios.post(baseURL, newContact)
   return request.then(response => {
     console.log('New contact added: ', response.data)
   })
 }
 
-const getDelete = (id, contact) => {
+export const getDelete = (id, contact) => {
   const request = axios.delete(`${baseURL}/${id}`, contact)
   return request.then(console.log('Contact list has been updated'))
 }
 
-const update = (id, contact) => {
+export const update = (id, contact) => {
   const request = axios.put(`${baseURL}/${id}`, contact)
   return request.then(console.log('Contact has been updated'))
 }
