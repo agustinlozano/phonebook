babd78cf1cdcf1523b8fb979ea702d5b46d8b7a7
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-01-28 21:49:51 -0300

    Fix update contact from handleAddContact

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const App = () => {
         /* Reemplazar los los datos del contacto en el servidor */
         const ID = findID(newContact, contacts)
         contactsServices
-          .update(ID, newContact)
+          .update(ID, newContacts)
           .catch(error => {
             const errorMessage = 'Error: contact could\'t be update'
             setNotification('failure-message')
