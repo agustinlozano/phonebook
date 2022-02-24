b7d77de75e419a715a37bd34eb9ea4bc78fbaea2
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-24 16:58:43 -0300

    Add a component with prop children

---
+++ src/components/Toggleable.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+const Toggleable = ({ children, buttonLabel }) => {
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setVisible(true)}>{buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {children}
+        <button onClick={() => setVisible(false)}>Cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default Toggleable
