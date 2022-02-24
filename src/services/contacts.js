6f66504a7a6e689ec5710ea3bc096caea0b3da0f
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-24 16:56:47 -0300

    Edit contact service: now supports users' token

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,25 +1,37 @@
 import axios from 'axios'
 
 /* Proviene del deploy de mi API */
-const baseURL = '/api/persons'
+const baseURL = 'http://localhost:3001/api/persons'
 
-export const getAll = () => axios.get(baseURL)
+let token = null
 
-export const create = newContact => {
-  const request = axios.post(baseURL, newContact)
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+const getAll = () => axios.get(baseURL)
+
+const create = newContact => {
+  const config = {
+    headers: {
+      Authorization: token
+    }
+  }
+  const request = axios.post(baseURL, newContact, config)
+
   return request.then(response => {
     console.log('New contact added: ', response.data)
   })
 }
 
-export const getDelete = (id, contact) => {
+const getDelete = (id, contact) => {
   const request = axios.delete(`${baseURL}/${id}`, contact)
   return request.then(console.log('Contact list has been updated'))
 }
 
-export const update = (id, contact) => {
+const update = (id, contact) => {
   const request = axios.put(`${baseURL}/${id}`, contact)
   return request.then(console.log('Contact has been updated'))
 }
 
-export default { getAll, create, getDelete, update }
+export default { getAll, create, getDelete, update, setToken }
