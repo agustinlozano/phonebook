5d531d3e48293d33c6400a51c58f7ea8e1e8b73c
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-26 14:16:30 -0300

    Update test

--- src/components/Contacts.test.js
+++ src/components/Contacts.test.js
@@ -1,31 +1,10 @@
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import { Contacts } from './Contacts'
+import { contactList } from '../utils/helper_test.js'
 
 describe(('<Contacts />'), () => {
   let component
-  const contactList = [
-    {
-      name: 'Arto Hellas',
-      phone: '040-123456',
-      id: 1
-    },
-    {
-      name: 'Ada Lovelace',
-      phone: '39-44-5323523',
-      id: 2
-    },
-    {
-      name: 'Dan Abramov',
-      phone: '12-43-234345',
-      id: 3
-    },
-    {
-      name: 'Mary Poppendieck',
-      phone: '39-23-6423122',
-      id: 4
-    }
-  ]
 
   const mockHandler = jest.fn()
 
