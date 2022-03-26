ee68c2830b515d4050a94ef067b663e919b32ce1
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-26 12:54:44 -0300

    Add a new test to Toggleable component

---
+++ src/components/Toggleable.test.js
@@ -0,0 +1,38 @@
+import '@testing-library/jest-dom/extend-expect'
+import { fireEvent, render } from '@testing-library/react'
+import Toggleable from './Toggleable'
+
+describe('<Toggleable />', () => {
+  let component
+
+  beforeEach(() => {
+    component = render(
+      <Toggleable buttonLabel='show'>
+        <div className='testDiv'>test div content</div>
+      </Toggleable>
+    )
+  })
+
+  test('renders its children', () => {
+    component.getByText('test div content')
+  })
+
+  test('at start the children are not displayed', () => {
+    const childrenElm = component.getByText('test div content')
+
+    expect(childrenElm.parentNode).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, children are displayed', () => {
+    const button = component.getByText('show')
+    fireEvent.click(button)
+
+    const childrenElm = component.getByText('test div content')
+    expect(childrenElm.parentNode).not.toHaveStyle('display: none')
+  })
+
+  test('cancel button can be clicked', () => {
+    const cancelButton = component.getByText('Cancel')
+    fireEvent.click(cancelButton)
+  })
+})
