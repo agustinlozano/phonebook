2df37ff3d67e33ee0178d398e4062dac966076ee
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-05-26 17:03:46 -0300

    Update the app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from 'react'
 import Contacts from './Contacts'
+import ContactForm from './ContactForm'
+import Notification from './Notification'
+
 import contactServices from '../services/contacts.js'
+import { showMessage } from '../utils/helper_methods'
 
 const App = () => {
   const [contacts, setContacts] = useState([])
+  const [typeNotification, setTypeNotification] = useState('')
+  const [notification, setNotification] = useState(null)
   const [user, setUser] = useState(false)
 
   useEffect(() => {
@@ -20,13 +26,38 @@ const App = () => {
       })
   }, [])
 
+  const addContact = (newContacts, typeNotification, notification) => {
+    setContacts(newContacts = contacts)
+    setTypeNotification(typeNotification)
+    showMessage(setNotification, notification)
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    contactServices.setToken(user.token)
+    window.localStorage.removeItem('loggedNoteAppUser')
+  }
+
   return (
     <>
       {
       user
-        ? <p>`Hola ${user}`</p>
+        ? (
+          <>
+            <p>`Hola ${user}`</p>
+            <ContactForm
+              contacts={contacts}
+              addContact={addContact}
+              handleLogout={handleLogout}
+            />
+          </>
+          )
         : <p>Hola Anonimo ^^</p>
       }
+      <Notification
+        message={notification}
+        type={typeNotification}
+      />
       <Contacts
         contacts={contacts}
       />
